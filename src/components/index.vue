9cc68ffde03fea458859f6901902f103ba1ada56
张敏 <zhangmin@zhangmindeMacBook-Air.local>
2018-03-28 22:11:09 +0800

    start

---
+++ src/components/index.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="container"> 
+        <div id="title">亿民惠食品厂</div>
+    <ul>
+      <li><a href="">客户管理</a></li>
+      <li><a href="">订单管理</a></li>
+      <li><a href="">产品管理</a></li>
+      <li><a href="">数据分析</a></li>
+    </ul>
+     </div> 
+</template>
+
+
+<script>
+  export default {
+    methods: {
+     
+    }
+  }
+</script>
+
+<style>
+#container{
+    height: 100%;
+    font-size: 30px;
+    color:#333;
+}
+#title{
+    width: 100%;
+    height: 15%;
+    display: inline-block;
+    vertical-align: middle;
+}
+ul{
+    width: 20%;
+    height: 84%;
+    text-align: center;
+    margin:0;
+    padding: 0;
+}
+li{
+    width: 100%;
+    height: 25%;
+    list-style: none;
+    border: 1px solid #ccc;
+    margin-bottom: -1px;
+     line-height: 150px; 
+}
+li a{
+    display: inline-block;
+    height: 100%;
+    width: 100%;
+    text-decoration: none;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '@/components/index'
+import Customer from '@/components/customer'
+import Order from '@/components/order'
+import Products from '@/components/order'
+import Data from '@/components/order'
+
 
 Vue.use(Router)
 
@@ -8,8 +13,26 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Index,
+      children:[
+        {
+          path:"customer",
+          component: Customer
+        },
+        {
+          path:"order",
+          component: Order
+        },
+        {
+          path:"products",
+          component: Products
+        },
+        {
+          path:"data",
+          component: Data
+        }
+      ]
+
     }
   ]
 })
