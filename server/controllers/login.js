6d93a06ed4bb34869ee741007e3be3e98797edf5
zhangmin <530023877@qq.com>
2018-05-09 09:17:08 +0800

    Update login.js

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,32 +1,15 @@
 // 登录授权接口
+const mysql = require('../tools/dbConnect');
+
 module.exports = async (ctx, next) => {
-const title = ctx.request.body.title;
-//   // const content = ctx.request.body.content;
-//   // const abstract = ctx.request.body.abstract;
-//   // const publish = ctx.request.body.publish;
-//   // const tags = ctx.request.body.tags;
-//   const createTime = new Date();
-//   const lastEditTime = new Date();
-//   if (title == '') {
-//     ctx.throw(400, '标题不能为空')
-//   }
-//   if (content == '') {
-//     ctx.throw(400, '文章内容不能为空')
-//   }
-//   if (abstract == '') {
-//     ctx.throw(400, '摘要不能为空')
-//   }
-//   const article = "";
-//    let createResult = "";
-//   // let createResult = await article.save().catch(err => {
-//   //   ctx.throw(500, '服务器内部错误')
-//   // });
-// //   await Article.populate(createResult, { path: 'tags' }, function(err, result) {
-// //     createResult = result;
-// //     // console.log(result)
 
-// //   });
-  console.log(title);
-  ctx.body = "hello word";
+let user = ctx.request.body.user;
+let pwd = ctx.request.body.pwd
 
+let data = await mysql("userMaster").select("*").where({"userId":user,"password":pwd})
+  if(data.length!==0){
+    ctx.response.body = "success"
+  }else{
+    ctx.response.body = "error"
+  }
 }
